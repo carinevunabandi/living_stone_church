d0df34f1f27491019742674ed0f7b157f45eb0e5
Carine Vunabandi <carine.vunabandi@bt.com>
2015-04-15 10:34:05 +0100

    Expect to see tab links instead of plain texts

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -7,27 +7,27 @@ Given 'I am on the homepage' do
 end
 
 Then 'I should see the Home tab' do
-  expect(page).to have_content('Home')
+  expect(page).to have_link('Home')
 end
 
 Then 'I should see the About Us tab' do
-  expect(page.body).to have_content('About Us')
+  expect(page.body).to have_link('About Us')
 end
 
 And 'I should see the Our Leaders tab' do
-  expect(page.body).to have_content('Our Leaders')
+  expect(page.body).to have_link('Our Leaders')
 end
 
 And 'I should see the Events tab' do
-  expect(page.body).to have_content('Events')
+  expect(page.body).to have_link('Events')
 end
 
 And 'I should see the Sermons tab' do
-  expect(page.body).to have_content('Sermons')
+  expect(page.body).to have_link('Sermons')
 end
 
 And 'I should see the Contact Us tab' do
-  expect(page.body).to have_content('Contact Us')
+  expect(page.body).to have_link('Contact Us')
 end
 
 And 'I should the Registered Charity footer note' do
@@ -35,5 +35,5 @@ And 'I should the Registered Charity footer note' do
 end
 
 And 'I should see the Gallery tab' do
-  expect(page.body).to have_content('Gallery')
+  expect(page.body).to have_link('Gallery')
 end
